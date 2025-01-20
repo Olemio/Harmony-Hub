e811f58e8ac019b1ee36e333479c95c44985ba2c
Oliver Bing <ailurus842@gmail.com>
2025-01-20 15:38:20 +0100

    add aws lambda function url for sending requests

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import type { ActionFunction, MetaFunction } from "@remix-run/node";
-import { useFetcher } from "@remix-run/react";
+import {
+  FetcherWithComponents,
+  useActionData,
+  useFetcher,
+} from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,49 +17,99 @@ export const action: ActionFunction = async ({ request }) => {
   const genre = formData.get("genre");
   const tempo = formData.get("tempo");
   const theme = formData.get("theme");
-  const songAmount = formData.get("songAmount");
+  const num_songs = formData.get("songAmount");
+  const endpointUrl =
+    "https://sermgkrheom6gele4rqbui5mlq0ffvan.lambda-url.eu-central-1.on.aws";
 
-  console.log({ genre, tempo, theme, songAmount });
+  console.log({ genre, tempo, theme, num_songs });
 
-  return { success: true };
+  const params = { genre, tempo, theme, num_songs };
+
+  try {
+    const url = new URL(endpointUrl);
+    Object.entries(params).forEach(([key, value]) => {
+      if (value) {
+        url.searchParams.append(key, String(value));
+      }
+    });
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return { error: error };
+  }
 };
 
 export default function Home() {
   const fetcher = useFetcher();
+  const state = fetcher.state;
+  const actionData = useActionData();
+  console.log(actionData);
+
   return (
     <div className=" flex flex-col items-center justify-center h-full gap-16">
+      {state === "submitting" ? (
+        <LoadingState />
+      ) : actionData && !actionData.error ? (
+        <Results data={actionData} />
+      ) : (
+        <SearchForm fetcher={fetcher} />
+      )}
+    </div>
+  );
+}
+
+function Results({ data }: { data: any }) {
+  return (
+    <div className="flex flex-col items-center gap-8">
+      <h1 className="text-3xl">Recommendations</h1>
+      <ul className="text-xl text-grayPrimary">
+        {data.recommendations.map((rec: any, index: number) => (
+          <li key={index} className="mb-2">
+            {rec.artist} - {rec.song}
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => window.location.reload()}
+        className="bg-greenPrimary text-grayPrimary rounded-full m-2 px-16 py-2 text-2xl font-bold"
+      >
+        Search Again
+      </button>
+    </div>
+  );
+}
+
+function LoadingState() {
+  return <h1 className="text-3xl">Searching...</h1>;
+}
+
+function SearchForm({ fetcher }: { fetcher: FetcherWithComponents<any> }) {
+  return (
+    <>
       <h1 className="text-3xl">Fill out form and search!</h1>
       <fetcher.Form method="POST" className="grid grid-cols-2 gap-10 text-xl">
-        <div className="flex flex-col items-center gap-2">
-          <label htmlFor="genre">Genre:</label>
-          <input
-            name="genre"
-            id="genre"
-            className="bg-greenSecondary rounded h-12 p-2 text-center text-grayPrimary"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <label htmlFor="genre">Tempo:</label>
-          <input
-            name="tempo"
-            className="bg-greenSecondary rounded h-12 p-2 text-center text-grayPrimary"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <label htmlFor="genre">Theme:</label>
-          <input
-            name="theme"
-            className="bg-greenSecondary rounded h-12 p-2 text-center text-grayPrimary"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <label htmlFor="genre">Amount of songs:</label>
-          <input
-            type="number"
-            name="songAmount"
-            className="bg-greenSecondary rounded h-12 p-2 text-center text-grayPrimary"
-          />
-        </div>
+        <FormInput name="genre" label="Genre" defaultValue="pop" />
+        <FormInput name="tempo" label="Tempo" defaultValue="upbeat" />
+        <FormInput name="theme" label="Theme" defaultValue="happy" />
+        <FormInput
+          name="songAmount"
+          label="Amount of songs"
+          defaultValue="2"
+          type="number"
+        />
+
         <div className="col-span-2 flex justify-center">
           <button
             type="submit"
@@ -65,6 +119,31 @@ export default function Home() {
           </button>
         </div>
       </fetcher.Form>
+    </>
+  );
+}
+
+function FormInput({
+  name,
+  label,
+  defaultValue,
+  type = "text",
+}: {
+  name: "songAmount" | "genre" | "tempo" | "theme";
+  label: string;
+  defaultValue: string | number;
+  type?: string;
+}) {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <label htmlFor={name}>{label}:</label>
+      <input
+        name={name}
+        id={name}
+        defaultValue={defaultValue}
+        type={type}
+        className="bg-greenSecondary rounded h-12 p-2 text-center text-grayPrimary"
+      />
     </div>
   );
 }
