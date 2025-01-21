8e94e69b093f984dfa5b5527f69ec78d70283d53
Oliver Bing <ailurus842@gmail.com>
2025-01-21 16:08:36 +0100

    add more mock data. render data in dashboard with styles and scrollable savd lists if list is too long

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,8 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-// import { mockData } from "../../mock";
-import { Item } from "../../types";
+import { mockData } from "../../mock";
+import { Item, SongData } from "../../types";
+import React from "react";
 
 export const loader: LoaderFunction = async () => {
   const url = "https://sobp5p7w82.execute-api.eu-central-1.amazonaws.com/items";
@@ -31,22 +32,52 @@ export const loader: LoaderFunction = async () => {
     );
   }
 };
-
 export default function Dashboard() {
   const loaderData = useLoaderData<Item[]>();
+  const [selectedList, setSelectedList] = React.useState<SongData | null>(null);
 
-  console.log(loaderData);
+  function handleSelectList(name: string) {
+    const list = mockData.find((item) => item.name === name);
+    setSelectedList(list || null);
+  }
 
   return (
-    <div>
-      <h1>Items in Database</h1>
-      <ul>
-        {loaderData[0].map((item) => (
-          <li key={item.id}>
-            <strong>{item.name}</strong>: {item.price} NOK
-          </li>
-        ))}
-      </ul>
+    <div className="flex gap-8 w-full justify-around px-16 items-start">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl">Songs</h1>
+        {selectedList ? (
+          <ul className="flex flex-col items-center justify-center gap-4">
+            {selectedList.recommendations.map((song, index) => (
+              <div key={index} className="flex items-center gap-2 ">
+                {index + 1}.
+                <li className="flex items-center justify-center bg-greenSecondary rounded h-10 px-4 text-grayPrimary">
+                  {song.song} by {song.artist}
+                </li>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-gray-500">
+            No list selected. Please select a list.
+          </p>
+        )}
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl">Saved lists</h1>
+        <ul className="flex flex-col items-center justify-center gap-4 overflow-y-auto max-h-96">
+          {mockData.map((item, index) => (
+            <li key={index}>
+              <button
+                onClick={() => handleSelectList(item.name)}
+                className="bg-greenPrimary text-grayPrimary rounded-full px-8 py-1 text-xl"
+              >
+                {item.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- mock.ts
+++ mock.ts
@@ -5,16 +5,14 @@ export const mockData = [
       { artist: "Pharrell Williams", song: "Happy" },
       { artist: "Justin Timberlake", song: "Can't Stop the Feeling!" },
       { artist: "Mark Ronson ft. Bruno Mars", song: "Uptown Funk" },
-      { artist: "Taylor Swift", song: "Shake It Off" }
-    ]
+    ],
   },
   {
     name: "My rock list!",
     recommendations: [
       { artist: "Queen", song: "We Will Rock You" },
       { artist: "Bon Jovi", song: "Livin' on a Prayer" },
-      { artist: "AC/DC", song: "Thunderstruck" }
-    ]
+    ],
   },
   {
     name: "My jazz list!",
@@ -23,8 +21,7 @@ export const mockData = [
       { artist: "John Coltrane", song: "My Favorite Things" },
       { artist: "Billie Holiday", song: "Blue Moon" },
       { artist: "Herbie Hancock", song: "Cantaloupe Island" },
-      { artist: "Duke Ellington", song: "In a Sentimental Mood" }
-    ]
+    ],
   },
   {
     name: "My hip-hop list!",
@@ -32,7 +29,137 @@ export const mockData = [
       { artist: "Eminem", song: "Lose Yourself" },
       { artist: "Kanye West", song: "Stronger" },
       { artist: "Kendrick Lamar", song: "HUMBLE." },
-      { artist: "Drake", song: "God's Plan" }
-    ]
-  }
+      { artist: "Drake", song: "God's Plan" },
+      { artist: "Jay-Z", song: "99 Problems" },
+    ],
+  },
+  {
+    name: "My country list!",
+    recommendations: [
+      { artist: "Johnny Cash", song: "Ring of Fire" },
+      { artist: "Dolly Parton", song: "Jolene" },
+      { artist: "Garth Brooks", song: "Friends in Low Places" },
+      { artist: "Shania Twain", song: "Man! I Feel Like a Woman!" },
+    ],
+  },
+  {
+    name: "My EDM list!",
+    recommendations: [
+      { artist: "Avicii", song: "Levels" },
+      { artist: "Martin Garrix", song: "Animals" },
+    ],
+  },
+  {
+    name: "My classical list!",
+    recommendations: [
+      { artist: "Ludwig van Beethoven", song: "Symphony No. 9" },
+      { artist: "Wolfgang Amadeus Mozart", song: "Eine kleine Nachtmusik" },
+      { artist: "Johann Sebastian Bach", song: "Toccata and Fugue in D minor" },
+    ],
+  },
+  {
+    name: "My indie list!",
+    recommendations: [
+      { artist: "Arctic Monkeys", song: "Do I Wanna Know?" },
+      { artist: "The 1975", song: "Somebody Else" },
+      { artist: "Tame Impala", song: "The Less I Know the Better" },
+      { artist: "Vampire Weekend", song: "A-Punk" },
+    ],
+  },
+  {
+    name: "My metal list!",
+    recommendations: [
+      { artist: "Metallica", song: "Enter Sandman" },
+      { artist: "Iron Maiden", song: "The Trooper" },
+    ],
+  },
+  {
+    name: "My R&B list!",
+    recommendations: [
+      { artist: "Beyoncé", song: "Halo" },
+      { artist: "Usher", song: "Yeah!" },
+      { artist: "Alicia Keys", song: "Fallin'" },
+    ],
+  }, {
+    name: "My pop list!",
+    recommendations: [
+      { artist: "Pharrell Williams", song: "Happy" },
+      { artist: "Justin Timberlake", song: "Can't Stop the Feeling!" },
+      { artist: "Mark Ronson ft. Bruno Mars", song: "Uptown Funk" },
+    ],
+  },
+  {
+    name: "My rock list!",
+    recommendations: [
+      { artist: "Queen", song: "We Will Rock You" },
+      { artist: "Bon Jovi", song: "Livin' on a Prayer" },
+    ],
+  },
+  {
+    name: "My jazz list!",
+    recommendations: [
+      { artist: "Miles Davis", song: "So What" },
+      { artist: "John Coltrane", song: "My Favorite Things" },
+      { artist: "Billie Holiday", song: "Blue Moon" },
+      { artist: "Herbie Hancock", song: "Cantaloupe Island" },
+    ],
+  },
+  {
+    name: "My hip-hop list!",
+    recommendations: [
+      { artist: "Eminem", song: "Lose Yourself" },
+      { artist: "Kanye West", song: "Stronger" },
+      { artist: "Kendrick Lamar", song: "HUMBLE." },
+      { artist: "Drake", song: "God's Plan" },
+      { artist: "Jay-Z", song: "99 Problems" },
+    ],
+  },
+  {
+    name: "My country list!",
+    recommendations: [
+      { artist: "Johnny Cash", song: "Ring of Fire" },
+      { artist: "Dolly Parton", song: "Jolene" },
+      { artist: "Garth Brooks", song: "Friends in Low Places" },
+      { artist: "Shania Twain", song: "Man! I Feel Like a Woman!" },
+    ],
+  },
+  {
+    name: "My EDM list!",
+    recommendations: [
+      { artist: "Avicii", song: "Levels" },
+      { artist: "Martin Garrix", song: "Animals" },
+    ],
+  },
+  {
+    name: "My classical list!",
+    recommendations: [
+      { artist: "Ludwig van Beethoven", song: "Symphony No. 9" },
+      { artist: "Wolfgang Amadeus Mozart", song: "Eine kleine Nachtmusik" },
+      { artist: "Johann Sebastian Bach", song: "Toccata and Fugue in D minor" },
+    ],
+  },
+  {
+    name: "My indie list!",
+    recommendations: [
+      { artist: "Arctic Monkeys", song: "Do I Wanna Know?" },
+      { artist: "The 1975", song: "Somebody Else" },
+      { artist: "Tame Impala", song: "The Less I Know the Better" },
+      { artist: "Vampire Weekend", song: "A-Punk" },
+    ],
+  },
+  {
+    name: "My metal list!",
+    recommendations: [
+      { artist: "Metallica", song: "Enter Sandman" },
+      { artist: "Iron Maiden", song: "The Trooper" },
+    ],
+  },
+  {
+    name: "My R&B list!",
+    recommendations: [
+      { artist: "Beyoncé", song: "Halo" },
+      { artist: "Usher", song: "Yeah!" },
+      { artist: "Alicia Keys", song: "Fallin'" },
+    ],
+  },
 ];
