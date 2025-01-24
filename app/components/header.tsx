ac6596d52efd37cd9812c1a732cda4258f2e2107
Oliver Bing <ailurus842@gmail.com>
2025-01-24 16:30:13 +0100

    use auth. instead of reveerse object thing

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,21 +5,15 @@ import React from "react";
 
 export default function Header() {
   const { pathname } = useLocation();
-  const {
-    isAuthenticated,
-    isLoading,
-    signinSilent,
-    signinRedirect,
-    signoutSilent,
-  } = useAuth();
+  const auth = useAuth();
 
   React.useEffect(() => {
-    if (!isAuthenticated && !isLoading) {
-      signinSilent().catch((err) => {
+    if (!auth.isAuthenticated && !auth.isLoading) {
+      auth.signinSilent().catch((err) => {
         console.error("Silent signin failed", err);
       });
     }
-  }, [isAuthenticated, isLoading, signinSilent]);
+  }, [auth]);
 
   return (
     <header className="flex justify-between items-center py-8 px-16 text-customPink bg-customDarkGray">
@@ -32,9 +26,11 @@ export default function Header() {
       <div className="flex">
         <Button
           className="text-customPink bg-transparent px-0 py-0 text-2xl"
-          onClick={() => (isAuthenticated ? signoutSilent() : signinRedirect())}
+          onClick={() =>
+            auth.isAuthenticated ? auth.signoutSilent() : auth.signinRedirect()
+          }
         >
-          {!isAuthenticated ? "Sign in" : "Sign out"}
+          {!auth.isAuthenticated ? "Sign in" : "Sign out"}
         </Button>
 
         <Link to={pathname !== "/" ? "/" : "/dashboard"}>
