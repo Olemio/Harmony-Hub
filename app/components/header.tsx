e816c6edb33d19a7dc0a36195402e84aa2076647
Oliver Bing <ailurus842@gmail.com>
2025-01-25 17:57:01 +0100

    display user name when signed in. Add dropdown menu to sign out

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,6 +7,7 @@ export default function Header() {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const auth = useAuth();
+  const [isOpen, setIsOpen] = React.useState(false);
 
   React.useEffect(() => {
     if (!auth.isAuthenticated && !auth.isLoading) {
@@ -32,16 +33,12 @@ export default function Header() {
   }, [auth.isAuthenticated, auth.user]);
 
   const handleSignOut = async () => {
-    if (auth.isAuthenticated) {
-      try {
-        await fetch("/auth/clear-token", { method: "POST" });
-        await auth.signoutSilent();
-        pathname === "/dashboard" && navigate("/");
-      } catch (err) {
-        console.error("Sign out failed:", err);
-      }
-    } else {
-      auth.signinRedirect();
+    try {
+      await fetch("/auth/clear-token", { method: "POST" });
+      await auth.signoutSilent();
+      pathname === "/dashboard" && navigate("/");
+    } catch (err) {
+      console.error("Sign out failed:", err);
     }
   };
 
@@ -52,19 +49,51 @@ export default function Header() {
           <h1 className="text-2xl">Harmony Hub</h1>
         </Link>
       </div>
-      <div className="flex gap-8">
-        <Button
-          className="text-customPink bg-transparent"
-          onClick={handleSignOut}
-        >
-          {!auth.isAuthenticated ? "Sign in" : "Sign out"}
-        </Button>
+      <div className="relative">
+        <div className="flex gap-10 items-center">
+          {!auth.isAuthenticated ? (
+            <Button
+              className="text-customPink bg-transparent"
+              onClick={() => auth.signinRedirect()}
+            >
+              Sign in
+            </Button>
+          ) : (
+            <>
+              <div className="relative">
+                <Button
+                  className="text-customPink bg-transparent"
+                  onClick={() => setIsOpen((prev) => !prev)}
+                >
+                  {auth.user?.profile.email?.split("@")[0]}
+                </Button>
 
-        <Link to={pathname !== "/" ? "/" : "/dashboard"}>
-          <Button className="text-lg px-4 py-1">
-            {pathname === "/" ? "Saved lists" : "Home"}
-          </Button>
-        </Link>
+                {isOpen && (
+                  <div className="absolute left-1/2 transform -translate-x-1/2 mt-4 w-40 bg-customDarkGray rounded-lg border border-black text-lg">
+                    <Button
+                      className="w-full text-center px-4 py-2 text-red-400 bg-transparent"
+                      onClick={handleSignOut}
+                    >
+                      Sign out
+                    </Button>
+                    <Button
+                      className="w-full text-center px-4 py-2 text-customPink bg-transparent border-t border-black rounded-none"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+
+          <Link to={pathname !== "/" ? "/" : "/dashboard"}>
+            <Button className="text-lg px-4 py-1">
+              {pathname === "/" ? "Saved lists" : "Home"}
+            </Button>
+          </Link>
+        </div>
       </div>
     </header>
   );
